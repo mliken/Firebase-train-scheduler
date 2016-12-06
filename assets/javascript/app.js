2ee53f5e7e839b5b3fb162ee24b1ffa2ae90e182
megan liken <meganliken@megans-MacBook-Pro-2.local>
2016-12-05 22:49:30 -0600

    firebase and beginnig html

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD32MYS-KKS0L47d43D_UMYDDN43Ry9vss",
+    authDomain: "train-scheduler-b3b6c.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b3b6c.firebaseio.com",
+    storageBucket: "train-scheduler-b3b6c.appspot.com",
+    messagingSenderId: "406994365282"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
